849c828c0d7690a1a529e6c8f602b5b59e3ce5d2
sigged <sderdeyn@gmail.com>
2019-10-12 20:32:43 +0200

    Add simple View Models
    (cherry picked from commit 6374bf25c2eafd2a1d8b0cf9128c8cd6a4f0c388)

---
+++ XrnCourse.MvvmBasics/XrnCourse.MvvmBasics/ViewModels/ClassmateViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace XrnCourse.MvvmBasics.ViewModels
+{
+    public class ClassmateViewModel
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public int Age
+        {
+            get
+            {
+                int age = DateTime.Now.Year - BirthDate.Year;
+                if (BirthDate > DateTime.Now.AddYears(-age)) age--; //adjust for leap year
+                return age;
+            }
+        }
+    }
+}

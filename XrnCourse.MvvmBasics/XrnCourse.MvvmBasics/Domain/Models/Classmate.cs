3c4c381a75a72e1894c4ebf7f324d9df4789640d
sigged <sderdeyn@gmail.com>
2019-10-12 17:11:04 +0200

    Add Classmate entity

---
+++ XrnCourse.MvvmBasics/XrnCourse.MvvmBasics/Domain/Models/Classmate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace XrnCourse.MvvmBasics.Domain.Models
+{
+    public class Classmate
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public DateTime Birthdate { get; set; }
+    }
+
+}

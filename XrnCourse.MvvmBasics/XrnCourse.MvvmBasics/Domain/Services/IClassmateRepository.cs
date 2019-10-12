290d352a43aa9011a682a133b29b2a37422a8d78
sigged <sderdeyn@gmail.com>
2019-10-12 17:40:27 +0200

    Add Classmate repository interface and JSON implementation

---
+++ XrnCourse.MvvmBasics/XrnCourse.MvvmBasics/Domain/Services/IClassmateRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using XrnCourse.MvvmBasics.Domain.Models;
+
+namespace XrnCourse.MvvmBasics.Domain.Services
+{
+    public interface IClassmateRepository
+    {
+        bool DatastoreExists { get; }
+
+        Task<IQueryable<Classmate>> GetAll();
+
+        Task<Classmate> GetClassmate(Guid id);
+
+        Task<Classmate> UpdateClassmate(Classmate classmate);
+
+        Task<Classmate> AddClassmate(Classmate classmate);
+
+        Task<Classmate> DeleteClassmate(Guid id);
+    }
+}

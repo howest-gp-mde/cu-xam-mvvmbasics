67830dad9f5d6e6a22964de1101039e1e59b0d43
sigged <sderdeyn@gmail.com>
2019-10-12 20:39:29 +0200

    Implement INotifyPropertyChanged in ViewModels
    (cherry picked from commit 5180f53241d523e92632a8117f289c67a435090d)

--- XrnCourse.MvvmBasics/XrnCourse.MvvmBasics/ViewModels/ClassmateViewModel.cs
+++ XrnCourse.MvvmBasics/XrnCourse.MvvmBasics/ViewModels/ClassmateViewModel.cs
@@ -1,21 +1,61 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace XrnCourse.MvvmBasics.ViewModels
 {
-    public class ClassmateViewModel
+    public class ClassmateViewModel : INotifyPropertyChanged
     {
-        public string Name { get; set; }
-        public string Phone { get; set; }
-        public DateTime BirthDate { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
 
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                RaisePropertyChanged(nameof(Name));
+            }
+        }
+        private string phone;
+        public string Phone
+        {
+            get { return phone; }
+            set
+            {
+                phone = value;
+                RaisePropertyChanged(nameof(Phone));
+            }
+        }
+        private DateTime birthdate;
+        public DateTime Birthdate
+        {
+            get { return birthdate; }
+            set
+            {
+                birthdate = value;
+                RaisePropertyChanged(nameof(Birthdate));
+            }
+        }
         public int Age
         {
             get
             {
-                int age = DateTime.Now.Year - BirthDate.Year;
-                if (BirthDate > DateTime.Now.AddYears(-age)) age--; //adjust for leap year
+                int age = DateTime.Now.Year - birthdate.Year;
+                if (birthdate > DateTime.Now.AddYears(-age)) age--; //adjust for leap year
                 return age;
             }
         }
+
+        /// <summary>
+        /// Raises the PropertyChanged event if it has handlers attached to it
+        /// </summary>
+        /// <param name="propertyName">name of the prop that was changed</param>
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
     }
 }
--- XrnCourse.MvvmBasics/XrnCourse.MvvmBasics/ViewModels/MainViewModel.cs
+++ XrnCourse.MvvmBasics/XrnCourse.MvvmBasics/ViewModels/MainViewModel.cs
@@ -1,10 +1,32 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
 using XrnCourse.MvvmBasics.Domain.Models;
 
 namespace XrnCourse.MvvmBasics.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
-        List<Classmate> Classmates { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private ObservableCollection<Classmate> classmates;
+        public ObservableCollection<Classmate> Classmates
+        {
+            get { return classmates; }
+            set
+            {
+                classmates = value;
+                RaisePropertyChanged(nameof(Classmates));
+            }
+        }
+
+        /// <summary>
+        /// Raises the PropertyChanged event if it has handlers attached to it
+        /// </summary>
+        /// <param name="propertyName">name of the prop that was changed</param>
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }

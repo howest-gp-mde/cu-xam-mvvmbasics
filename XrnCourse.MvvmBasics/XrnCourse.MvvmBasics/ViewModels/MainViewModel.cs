6374bf25c2eafd2a1d8b0cf9128c8cd6a4f0c388
sigged <sderdeyn@gmail.com>
2019-10-12 20:32:43 +0200

    Add simple View Models

---
+++ XrnCourse.MvvmBasics/XrnCourse.MvvmBasics/ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using XrnCourse.MvvmBasics.Domain.Models;
+
+namespace XrnCourse.MvvmBasics.ViewModels
+{
+    public class MainViewModel
+    {
+        List<Classmate> Classmates { get; set; }
+    }
+}

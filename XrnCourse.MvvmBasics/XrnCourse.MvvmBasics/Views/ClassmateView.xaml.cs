c7044a849c2a6902997be0259fecb65d7aa0eab8
sigged <sderdeyn@gmail.com>
2019-10-13 21:13:39 +0200

    Pass Xamarins Navigation instance to the View Model

--- XrnCourse.MvvmBasics/XrnCourse.MvvmBasics/Views/ClassmateView.xaml.cs
+++ XrnCourse.MvvmBasics/XrnCourse.MvvmBasics/Views/ClassmateView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XrnCourse.MvvmBasics.Domain.Models;
 using XrnCourse.MvvmBasics.ViewModels;
 
 namespace XrnCourse.MvvmBasics.Views
@@ -7,10 +8,10 @@ namespace XrnCourse.MvvmBasics.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ClassmateView : ContentPage
     {
-        public ClassmateView()
+        public ClassmateView(Classmate classmate)
         {
             InitializeComponent();
-            BindingContext = new ClassmateViewModel();
+            BindingContext = new ClassmateViewModel(classmate, this.Navigation);
         }
     }
 }

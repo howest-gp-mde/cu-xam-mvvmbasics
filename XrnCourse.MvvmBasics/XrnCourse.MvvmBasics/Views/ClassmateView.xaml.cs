8ca341443a9669946e3061f0ed8eedb9c04c0d0e
sigged <sderdeyn@gmail.com>
2019-10-13 20:24:06 +0200

    Binds View Model properties to View elements
    (cherry picked from commit 3d81339bb36a7f7bf607570bfca7c35862a9aaf8)

--- XrnCourse.MvvmBasics/XrnCourse.MvvmBasics/Views/ClassmateView.xaml.cs
+++ XrnCourse.MvvmBasics/XrnCourse.MvvmBasics/Views/ClassmateView.xaml.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XrnCourse.MvvmBasics.ViewModels;
 
 namespace XrnCourse.MvvmBasics.Views
 {
@@ -15,6 +10,7 @@ namespace XrnCourse.MvvmBasics.Views
         public ClassmateView()
         {
             InitializeComponent();
+            BindingContext = new ClassmateViewModel();
         }
     }
 }

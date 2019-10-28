fb7fa3d5a4a06a4fd05a08d2f0fd70c4b9d4c673
sigged <sderdeyn@gmail.com>
2019-10-28 16:22:49 +0100

    Register view models in container to enable dependency injection in their constructor

--- XrnCourse.MvvmBasics/XrnCourse.MvvmBasics/IoC/IoCBuilder.cs
+++ XrnCourse.MvvmBasics/XrnCourse.MvvmBasics/IoC/IoCBuilder.cs
@@ -1,5 +1,6 @@
 ﻿using Autofac;
 using XrnCourse.MvvmBasics.Domain.Services;
+using XrnCourse.MvvmBasics.ViewModels;
 
 namespace XrnCourse.MvvmBasics.IoC
 {
@@ -9,6 +10,10 @@ namespace XrnCourse.MvvmBasics.IoC
         public static ContainerBuilder GetDefaultContainerBuilder()
         {
             var containerBuilder = new ContainerBuilder();
+
+            //registering these types enable dependency injection for their ctors
+            containerBuilder.RegisterType<MainViewModel>();
+            containerBuilder.RegisterType<ClassmateViewModel>();
 
             //always use a JsonClassmateRepository when a IClassmatRepository is needed
             containerBuilder.RegisterType<JsonClassmateRepository>().As<IClassmateRepository>();
